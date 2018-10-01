987b1b0079ebfc9fae21b068bd153ab262de0aad
sturnbull95 <steven.turnbull901@gmail.com>
2018-10-01 10:06:59 -0700

    original commit didnt work

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+
+    this.age = 10;
+    this.canFly = true;
+    this.job = 'find pollen';
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+
+    this.age = 40;
+    this.color = 'grey';
+    this.canFly = false;
+    this.job = 'gamble';
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble() {
+    this.treasureChest.push('Pollen chips');
+  }
+}
